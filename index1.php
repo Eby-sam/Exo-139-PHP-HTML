5fddba65ec43b00086451f15e1fcf41bce81f643
coque <coquelet.samuel@gmail.com>
2022-01-10 09:37:59 +0100

    fini

--- index1.php
+++ index1.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -7,15 +6,20 @@
   </head>
   <body>
     <h1>Liste des élèves</h1>
-    <!-- Instructions : Afficher la liste des élèves qui sont présents dans le tableau $students, enrobez les dans un div, un span ou un p -->
+
     <?php
-        //students
-        $students = ['Hulk', 'Iron Man', 'Wonder Woman', 'Black Widow', 'Malicia'];
-     ?>
-     <ul>
-       <?php
-            // TODO votre code ici
-       ?>
-     </ul>
+    //students
+        $students = ['sam', 'greg', 'sarah', 'sonia', 'tsunade'];
+    ?>
+    <ul>
+        <?php
+            $chaine = implode(", ", $students);
+            echo "<div>$chaine</div>";
+        ?>
+    </ul>
 
     <!-- Incluez le fichier index2.php ici bas de manière a obtenir la suite du code HTML. -->
+
+    <?php
+        include "index2.php";
+    ?>
--- index2.php
+++ index2.php
@@ -1,29 +1,39 @@
 <hr>
-     <h1>Date du jour</h1>
+     <h1>Date du jour (10/01/2022)</h1>
      <form>
 
        <!-- Instructions : Créer la liste de jour (en chiffres), de mois (en chiffres) et d'année en PHP. -->
-       <label for="day">Day</label>
-       <select  id="day">
-           <?php
-           // TODO list of day
-           ?>
-       </select>
+         <label for="day">Day</label>
+         <select  id="day">
+             <?php
+                 $day = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+                 for($i = 0; $i < 31; $i++){
+                     echo "<option>$day[$i] </option>";
+                 }
 
-       <label for="month">Month</label>
-       <select  id="month">
-           <?php
-           // TODO list of month
-           ?>
-       </select>
+             ?>
+         </select>
+
+         <label for="month">Month</label>
+         <select  id="month">
+             <?php
+                 $month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+                 for($i = 0; $i < 12; $i++){
+                     echo "<option>$month[$i] </option>";
+                 }
+             ?>
+         </select>
 
          <label for="year">Year</label>
-       <select  id="year">
-           <?php
-           // TODO list of year 2018, 2019, 2020 --> Bonus : faites le avec une vraie date en PHP, renseignez vous sur la doc
+         <select  id="year">
+             <?php
+                 $year = [2018, 2019, 2020, 2021, 2022];
+                 for($i = 0; $i < 5; $i++){
+                     echo "<option>$year[$i] </option>";
+                 }
 
-           ?>
-       </select>
+             ?>
+         </select>
      </form>
   </body>
 </html>
